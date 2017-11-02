5ec4c07216441de8f539fce024ee70c9d262210e
Danny Youm <dyoum@Veracode.com>
2017-11-02 15:38:45 -0400

    modification to TestFinal

--- TestFinal.java
+++ TestFinal.java
@@ -3,7 +3,9 @@ import java.util.List;
 
 
 // This class will compile and execute properly
-// HOWEVER< changing List foo to be private static final will cause compilation error
+// HOWEVER changing List foo to be private static final will cause compilation error
+// snippet of code taken from https://stackoverflow.com/questions/15655012/how-final-keyword-works
+
 class TestFinal {
   private final List foo;
 
@@ -14,9 +16,28 @@ class TestFinal {
   }
   public static void main(String[] args) 
   {
-      TestFinal t = new TestFinal();
-      t.foo.add("bar"); // Modification-2
-      System.out.println("print - " + t.foo);
+    TestFinal t = new TestFinal();
+  
+    t.foo.add("bar"); // Modification-2
+    System.out.println("print - " + t.foo);
   }
 }
 
+// Apparently, we can initialize a final variable and do so once!
+// but I assumed we couldn't add bar to it... How is this possible?
+// MISNOMER to assume that final means that variable can only be modified once
+// final refers to only the reference and not the contents of referenced object
+// t.foo = new ArrayList() after constructing t will result in compilation error
+
+
+// to better understand,
+// foo is an instance variable of the class TestFinal. When obj is created,
+// instance variable foo will be copied inside object of TestFinal class.
+// STATIC keyword is very different. If we have private final static List foo,
+// then foo is not copied to the object.
+// foo will instead be seen by MULTIPLE objects 
+
+
+// final class cannot be subclassed
+// final method cannot be overridden by subclasses
+// final variable can only be initialized once
